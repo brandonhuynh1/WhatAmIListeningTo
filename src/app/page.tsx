51921442fff008c80e57d2bcf815100f089e465f
brandonhuynh1 <brandon.huynh@live.com>
2024-09-04 18:27:25 -0700

    Updated loading symbol

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -53,16 +53,21 @@ export default function Home() {
             <span className="flex items-center justify-center">
               <svg
                 className="animate-spin h-5 w-5 mr-3 text-white"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
                 viewBox="0 0 24 24"
-                stroke="currentColor"
               >
+                <circle
+                  className="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  strokeWidth="4"
+                  fill="none"
+                />
                 <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M12 4v7l3 3m-3-3a8 8 0 11-8 8 8 8 0 018-8z"
+                  className="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
                 />
               </svg>
               Redirecting...
